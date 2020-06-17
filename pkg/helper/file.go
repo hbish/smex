a411545bab78c0612e4616a6060b317da57154e5
Ben Shi <ben.shi@growsuper.com>
2020-06-17 23:58:33 +1000

    chore: lint fix

--- pkg/helper/file.go
+++ pkg/helper/file.go
@@ -1,3 +1,6 @@
+//Package helper ...
+package helper
+
 /*
 Copyright © 2020 Ben Shi
 
@@ -19,7 +22,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
-package helper
 
 import (
 	"io/ioutil"
